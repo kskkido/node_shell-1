0433d74f58722c7656c49ebd0a3f499623466a34
Gabriel <gabriel.wrowe@gmail.com>
2017-03-06 11:26:52 -0500

    finished ls

--- bash.js
+++ bash.js
@@ -1,15 +1,7 @@
-process.stdout.write('prompt > ');
+var commands = require('./commands');
 
-// The stdin 'data' event fires after a user types in a line
+
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
-  if (cmd === "pwd")
-  	process.stdout.write(process.env.PWD);
-  if (cmd === "date") {
-  	var nowDate = new Date();
-  	process.stdout.write(nowDate.toString());
-  }
-
-  process.stdout.write('\nprompt > ');
-
-});
+	var cmd = data.toString().trim();
+	commands[cmd]();
+})
--- commands.js
+++ commands.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 module.exports = {
 
 
@@ -18,12 +20,27 @@ module.exports = {
 	date: function() {
 		process.stdout.write('prompt > ');
 		process.stdin.on('data', function (data) {
-	  	
+	  		var cmd = data.toString().trim();
 		  	if (cmd === "date") {
 			  	var nowDate = new Date();
 			  	process.stdout.write(nowDate.toString());
 		  	}
 		  	process.stdout.write('\nprompt > ');
+		})
+	},
+	ls: function() {
+		process.stdout.write('prompt > \n');
+		process.stdin.on('data', function (data) {
+	  		var cmd = data.toString().trim();
+		  	if (cmd === "ls") {
+			  	fs.readdir('.', function(err, files) {
+			  		if(err) throw err;
+			  		files.forEach(function(file) {
+			  			process.stdout.write(file.toString() + '\n');
+			  		})
+			  	})
+		  	}
+		  	// process.stdout.write('\nprompt > ');
 		})
 	}
 }
