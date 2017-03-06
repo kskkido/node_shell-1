0ee4d3ed7d0fa5296382d1a56440d4007d4c41a6
Keisuke Kido <kskkido@gmail.com>
2017-03-06 11:54:57 -0500

    echo

--- bash.js
+++ bash.js
@@ -4,5 +4,12 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', function (data) {
 	var cmd = data.toString().trim();
-	commands[cmd]();
+	if (cmd.slice(0, 4) === "echo") {
+		var echo = cmd.slice(0, 4);
+		var str = cmd.slice(5);
+		commands[echo](str);
+	}
+	else {
+		commands[cmd]();
+	}
 })
--- commands.js
+++ commands.js
@@ -6,25 +6,32 @@ module.exports = {
 
 
 	// The stdin 'data' event fires after a user types in a line
-	pwd: function() {
+	pwd: function(file) {
 	  	process.stdout.write(process.env.PWD);
 	  	process.stdout.write('\nprompt > ')
 	},
 
-	date: function() {
+	date: function(file) {
 	  	var nowDate = new Date();
 	  	process.stdout.write(nowDate.toString());
   		process.stdout.write('\nprompt > ');
 	},
-	ls: function() {
+	ls: function(file) {
 		fs.readdir('.', function(err, files) {
 	  		if(err) throw err;
 	  		files.forEach(function(file) {
-	  			process.stdout.write(file.toString() + '\n');
+	  			process.stdout.write(file.toString()+'\n');
 
 	  		})
 	  		process.stdout.write('\nprompt > ');
 	  	})
+	},
+	echo: function(str) {
+		process.stdout.write(str);
+  		process.stdout.write('\nprompt > ');
+	}
+	cat: function(file) {
+		
 	}
 }
 
